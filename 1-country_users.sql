dc519646b9607c1c6eb49f1d997a1ce6806e0127
Jamal <jayjamal86@gmail.com>
2023-04-05 16:05:24 +0300

    1-country_users.sql and 2-fans.sql

---
+++ 1-country_users.sql
@@ -0,0 +1,6 @@
+CREATE TABLE IF NOT EXISTS users (
+    id SERIAL PRIMARY KEY,
+    email VARCHAR(255) NOT NULL,
+    name VARCHAR(255),
+    country ENUM('US', 'CA', 'TN') NOT NULL DEFAULT 'US'
+    );
