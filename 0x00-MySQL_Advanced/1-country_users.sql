5297095a73a981002e19ee9fc7826b5fa1efc72d
Jamal <jayjamal86@gmail.com>
2023-04-09 00:18:15 +0300

    UNIQUE KEY

--- 0x00-MySQL_Advanced/1-country_users.sql
+++ 0x00-MySQL_Advanced/1-country_users.sql
@@ -1,7 +1,7 @@
 -- Create a table users with the following fields:
 CREATE TABLE IF NOT EXISTS users (
     id SERIAL PRIMARY KEY,
-    email UNIQUE VARCHAR(255) NOT NULL,
+    email VARCHAR(255) NOT NULL UNIQUE,
     name VARCHAR(255),
     country CHAR(2) NOT NULL DEFAULT 'US' CHECK (country IN ('US', 'CO', 'TN')),
     );
