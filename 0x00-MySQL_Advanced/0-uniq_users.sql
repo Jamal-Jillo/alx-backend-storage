f13219035e249391d7340fc6cc639c8077870769
Jamal <jayjamal86@gmail.com>
2023-04-08 23:42:56 +0300

    fixes #1

--- 0x00-MySQL_Advanced/0-uniq_users.sql
+++ 0x00-MySQL_Advanced/0-uniq_users.sql
@@ -1,4 +1,5 @@
--- Create a table called users with the following fields:.
+-- Create a table called users
+-- with the following fields:.
 DROP TABLE IF EXISTS users;
 CREATE TABLE users (
     id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
