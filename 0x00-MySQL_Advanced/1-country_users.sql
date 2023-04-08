5960b3559732320679c260ddd647f6f3f163ebee
Jamal <jayjamal86@gmail.com>
2023-04-09 00:16:35 +0300

    email should be unique

--- 0x00-MySQL_Advanced/1-country_users.sql
+++ 0x00-MySQL_Advanced/1-country_users.sql
@@ -1,7 +1,7 @@
 -- Create a table users with the following fields:
 CREATE TABLE IF NOT EXISTS users (
     id SERIAL PRIMARY KEY,
-    email VARCHAR(255) NOT NULL,
+    email UNIQUE VARCHAR(255) NOT NULL,
     name VARCHAR(255),
-    country ENUM('US', 'CA', 'TN') NOT NULL DEFAULT 'US'
+    country CHAR(2) NOT NULL DEFAULT 'US' CHECK (country IN ('US', 'CO', 'TN')),
     );
