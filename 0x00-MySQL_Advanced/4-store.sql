1a92d0101d6d4a31718b0dbd7b53bd9a5c81e6d9
Jamal <jayjamal86@gmail.com>
2023-04-09 00:24:58 +0300

    add delimiters to store.sql

--- 0x00-MySQL_Advanced/4-store.sql
+++ 0x00-MySQL_Advanced/4-store.sql
@@ -1,7 +1,12 @@
 -- Name: items; Type: TABLE; Schema: public; Owner: postgres
-CREATE TRIGGER decrease_quantity_trigger
+DROP TRIGGER IF EXISTS reduce_quantity;
+DELIMITER $$
+CREATE TRIGGER reduce_quantity
 AFTER INSERT ON orders
 FOR EACH ROW
-UPDATE items
-SET quantity = quantity - NEW.quantity
-WHERE id = NEW.item_id;
+BEGIN
+    UPDATE items
+        SET quantity = quantity - NEW.number
+        WHERE name = NEW.item_name;
+END $$
+DELIMITER ;
