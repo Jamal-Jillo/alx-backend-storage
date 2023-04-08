585b6ec61d2d3374af6d614b95e24e7a10525abc
Jamal <jayjamal86@gmail.com>
2023-04-09 00:22:44 +0300

    3-glam_rock.sql check 2 and 5 fixed

--- 0x00-MySQL_Advanced/3-glam_rock.sql
+++ 0x00-MySQL_Advanced/3-glam_rock.sql
@@ -1,6 +1,5 @@
 -- Name: 3-glam_rock.sql
-SELECT band_name, 
-       EXTRACT(YEAR FROM AGE(split, formed)) AS lifespan
-FROM metal_bands
-WHERE main_style = 'Glam rock'
-ORDER BY lifespan DESC;
+SELECT band_name, (IFNULL(split, '2020') - formed) AS lifespan
+    FROM metal_bands
+    WHERE FIND_IN_SET('Glam rock', IFNULL(style, "")) > 0
+    ORDER BY lifespan DESC;
