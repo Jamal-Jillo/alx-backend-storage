198f744673ac15997003db5ee415fcb2d65deea6
Jamal <jayjamal86@gmail.com>
2023-04-09 00:42:22 +0300

    task 8,9,10

--- 0x00-MySQL_Advanced/8-index_my_names.sql
+++ 0x00-MySQL_Advanced/8-index_my_names.sql
@@ -1,2 +1,2 @@
 -- create the index idx_name_first
-CREATE INDEX idx_name_first ON names (LEFT(name, 1));
+CREATE INDEX idx_name_first ON names(name(1));
--- 0x00-MySQL_Advanced/9-index_name_score.sql
+++ 0x00-MySQL_Advanced/9-index_name_score.sql
@@ -1,2 +1,2 @@
 -- Name: idx_name_first_score; Type: INDEX; Schema: alx; Owner: alx
-CREATE INDEX idx_name_first_score ON names (LEFT(name, 1), score);
+CREATE INDEX idx_name_first_score ON names(name(1), score);
